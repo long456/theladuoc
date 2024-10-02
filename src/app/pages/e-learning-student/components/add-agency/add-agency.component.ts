65dc516cfc768500bd09440924a53cccd92ac77e
Long Nguyen <nnlong.10.01@gmail.com>
2024-10-02 09:03:29 +0700

    feature: add agency module

---
+++ src/app/pages/e-learning-student/components/add-agency/add-agency.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-agency',
+  templateUrl: './add-agency.component.html',
+  styleUrls: ['./add-agency.component.scss']
+})
+export class AddAgencyComponent {
+
+}
---
+++ src/app/pages/e-learning-student/components/agency-list/agency-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agency-list',
+  templateUrl: './agency-list.component.html',
+  styleUrls: ['./agency-list.component.scss']
+})
+export class AgencyListComponent {
+
+}
